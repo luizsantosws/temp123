daa89b521dd911a6ef547e98bf6f8ead4cfb90f4
Luiz Santos <160616203+luizsantosws@users.noreply.github.com>
2025-03-27 23:12:08 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_HOST', '192.168.0.3');
-define('DB_PORT', '3331');
-define('DB_NAME', 'esp_data');
-define('DB_USER', 'espuser');
-define('DB_PASS', 'esppass');
+define('DB_HOST', '');
+define('DB_PORT', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
 date_default_timezone_set('America/Sao_Paulo');
 ?>
